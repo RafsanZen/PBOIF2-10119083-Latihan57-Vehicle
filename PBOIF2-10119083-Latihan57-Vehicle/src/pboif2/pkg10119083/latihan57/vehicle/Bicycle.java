bbcb31b2568731a01562313817c1b9d53751cc8f
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-22 01:26:27 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan57-Vehicle/src/pboif2/pkg10119083/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan57.vehicle;
+
+/**
+ *
+* NAMA        : Rafsam Zen Mustaofa
+ * KELAS       : PBOIF2
+ * NIM         : 10119083
+ * 
+ */
+public class Bicycle extends Vehicle{
+    private int myGearCount;
+
+    public Bicycle() {
+        System.out.println("Bicycle");
+    }
+
+    public int getMyGearCount() {
+        return myGearCount;
+    }
+
+    public void setMyGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+    
+}
